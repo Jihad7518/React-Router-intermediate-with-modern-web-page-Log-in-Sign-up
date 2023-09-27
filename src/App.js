2f9fcdbb34eecbef4f23db58753930ea9bfe82d6
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-28 01:26:42 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
+import { useEffect, useState } from 'react'
+import PrivateRoute from "./components/PrivateRoute";
 
+
+function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+ 
