fd81177c952b986fd8581a4d9ac2361909a860d2
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-29 01:51:27 +0600

    Update SignupForm.js

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -69,5 +69,20 @@ const SignupForm = ({setIsLoggedIn}) => {
             onClick={()=> setAccountType("student")}>
                 Student
             </button>
+<button
+            className={`${accountType === "instructor" 
+            ?
+              "bg-richblack-900 text-richblack-5"
+            :"bg-transparent text-richblack-200"} py-2 px-5 rounded-full transition-all duration-200`}
+            onClick={() => setAccountType("instructor")}>
+                Instructor
+            </button>
+        </div>
+
+        <form onSubmit={submitHandler} >
+        {/* first name and lastName */}
+            <div className='flex gap-x-4 mt-[20px]'>
+                    <label className='w-full'>
+                        <p className='text-[0.875rem] text-richblack-5 mb-1 leading-[1.375rem]'>First Name<sup className='text-pink-200'>*</sup></p>
 
 
