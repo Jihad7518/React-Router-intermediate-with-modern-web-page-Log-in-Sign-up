aed925890475915eb38c2d33a3dd396689bb159d
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-29 01:46:20 +0600

    Update Template.js

--- src/components/Template.js
+++ src/components/Template.js
@@ -1,1 +1,11 @@
+import React from 'react'
+import frameImage from "../assets/frame.png"
+import SignupForm from './SignupForm'
+import LoginForm from './LoginForm'
+import {FcGoogle} from "react-icons/fc"
 
+
+const Template = ({title, desc1, desc2, image, formtype, setIsLoggedIn}) => {
+
+    // console.log("ye rha mera form type");
+    // console.log(formtype)
