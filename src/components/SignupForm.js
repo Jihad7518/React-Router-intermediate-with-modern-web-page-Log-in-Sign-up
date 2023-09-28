e69434ce806cd7a60315fee79ca41858c87ae4c3
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-29 01:50:20 +0600

    Update SignupForm.js

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,1 +1,16 @@
+import React, { useState } from 'react'
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import {toast} from "react-hot-toast"
+import { useNavigate } from 'react-router-dom';
 
+
+const SignupForm = ({setIsLoggedIn}) => {
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        password:"",
+        confirmPassword:""
+    })
