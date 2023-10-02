424a538ea14ea29f3bdbad6d83b0229e0b6885e3
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-03 00:47:17 +0600

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,1 +1,14 @@
+import React, { useState } from 'react'
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+
+
+const LoginForm = ({setIsLoggedIn}) => {
+
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState( {
+        email:"", password:""
+    })
 
