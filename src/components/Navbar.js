290bf17facdfa30419a6a67050f4917ac74130e6
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-03 00:48:51 +0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,9 @@
+import React from 'react'
+import logo from "../assets/Logo.svg"
+import {Link} from "react-router-dom"
+import {toast} from "react-hot-toast"
 
+
+const Navbar = (props) => {
+    let isLoggedIn = props.isLoggedIn;
+    let setIsLoggedIn = props.setIsLoggedIn;
