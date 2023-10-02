78867dbfe9c2c21fc4713c17972060b65b7cf0be
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-03 00:47:48 +0600

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -32,3 +32,22 @@ const[showPassword, setShowPassword] = useState(false);
         console.log(formData)
         navigate("/dashboard");
     }
+    return (
+    <form onSubmit={submitHandler}
+    className="flex flex-col w-full gap-y-4 mt-6">
+
+        <label className='w-full'>
+            <p className='text-[0.875rem] text-richblack-5 mb-1 leading-[1.375rem]'>
+                Email Address<sup className='text-pink-200'>*</sup>
+            </p>
+            <input 
+                required
+                type="email"
+                value = {formData.email}
+                onChange={changeHandler}
+                placeholder="Enter email address"
+                name="email"
+                className='bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]'
+            />
+        </label>
+
